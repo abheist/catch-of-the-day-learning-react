276cd35e66dbce9c08a0c048209a983a2ff8c069
Abhishek <abhishek@kisanhub.com>
2016-12-08 14:27:37 +0530

    intermediate

--- scripts/main.js
+++ scripts/main.js
@@ -18,6 +18,10 @@ var App = React.createClass({
             order: {}
         }
     },
+    addToOrder: function(key) {
+        this.state.order[key] = this.state.order[key] + 1 || 1;
+        this.setState({ order: this.state.order });
+    },
     addFish: function (fish) {
         var timestamp = (new Date()).getTime();
         this.state.fishes['fish-' + timestamp] = fish;
@@ -29,7 +33,7 @@ var App = React.createClass({
         });
     },
     renderFish: function (key) {
-        return <Fish key={key} index={key} details={this.state.fishes[key]} />
+        return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
     },
     render: function () {
         return (
@@ -49,6 +53,11 @@ var App = React.createClass({
 
 
 var Fish = React.createClass({
+    onButtonClick: function () {
+        console.log("Hellos"+ this.props.index);
+        var key = this.props.index;
+        this.props.addToOrder(key);
+    },
     render: function () {
         var details = this.props.details;
         var isAvailable = (details.status == 'available' ? true : false);
@@ -61,7 +70,7 @@ var Fish = React.createClass({
                     <span className="price">{h.formatPrice(details.price)}</span>
                 </h3>
                 <p>{details.desc}</p>
-                <button disabled={!isAvailable}>{buttonText}</button>
+                <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
             </li>
         )
     }
