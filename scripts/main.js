559b13834b10983598274b13bf6b9dc9a7d6751a
Abhishek <abhishek291093@gmail.com>
2016-12-15 15:46:19 +0530

    Intermediate

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
 
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
@@ -45,10 +46,10 @@ var App = React.createClass({
         this.state.order[key] = this.state.order[key] + 1 || 1;
         this.setState({ order: this.state.order });
     },
-    removeFromOrder : function(key) {
+    removeFromOrder: function (key) {
         delete this.state.order[key];
         this.setState({
-            order : this.state.order
+            order: this.state.order
         });
     },
     addFish: function (fish) {
@@ -166,17 +167,22 @@ var Order = React.createClass({
         var fish = this.props.fishes[key];
         var count = this.props.order[key];
         var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times;</button>
-
         if (!fish) {
             return <li key={key}>Sorry fish no lionger available! {removeButton}</li>
         }
 
         return (
             <li key={key}>
-                {count}lbs
-                {fish.name}
+                <span>
+                    <CSSTransitionGroup component="span" transitionName="count"
+                        transitionEnterTimeout={250} transitionLeaveTimeout={250}>
+                        <span key={count}>{count}</span>
+                    </CSSTransitionGroup>
+
+                    lbs {fish.name} {removeButton}
+
+                </span>
                 <span className="price">{h.formatPrice(count * fish.price)}</span>
-                {removeButton}
             </li>
         )
     },
@@ -196,13 +202,21 @@ var Order = React.createClass({
         return (
             <div className="order-wrap">
                 <h2 className="order-title">Your Order</h2>
-                <ul className="order">
+
+                <CSSTransitionGroup
+                    className="order"
+                    component="ul"
+                    transitionName="order"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}
+                    >
                     {orderIds.map(this.renderOrder)}
                     <li className="total">
                         <strong>Total:</strong>
                         {h.formatPrice(total)}
                     </li>
-                </ul>
+                </CSSTransitionGroup>
+
             </div>
         )
     }
